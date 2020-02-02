4b1540aed0d492edf19c2655d171060a36e2d453
pierpuccini <pierpuccini@gmail.com>
2020-02-02 16:06:45 -0500

    Missing detailed parsing

--- Backend/controller/daviviendaController.js
+++ Backend/controller/daviviendaController.js
@@ -2,211 +2,266 @@ const path = require("path");
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const credentials = require("../../credentials.json");
-const cheerio = require('cheerio')
+const cheerio = require("cheerio");
+var HTMLParser = require('node-html-parser');
 
 const getReports = async (req, res) => {
   const { id, password } = credentials;
   console.log("[started DAVIVIENDA]");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  /* ----------------------------------- getting page ----------------------------------- */
-  await page.goto("https://www.davivienda.com/wps/portal/personas/nuevo");
+  let data = [];
+  try {
+    /* ----------------------------------- getting page ----------------------------------- */
+    await page.goto("https://www.davivienda.com/wps/portal/personas/nuevo");
 
-  /* ----------------------------------- clicking Login ----------------------------------- */
-  await page.click("#personas-ingresar");
+    /* ----------------------------------- clicking Login ----------------------------------- */
+    await page.click("#personas-ingresar");
 
-  /* ----------------------------------- waiting for iframe ----------------------------------- */
-  console.log("[waiting for iframe]");
-  await page.waitForSelector("div#divIFrame iframe");
-  const elementHandle = await page.$("div#divIFrame iframe");
+    /* ----------------------------------- waiting for iframe ----------------------------------- */
+    console.log("[waiting for iframe]");
+    await page.waitForSelector("div#divIFrame iframe");
+    const elementHandle = await page.$("div#divIFrame iframe");
 
-  /* ----------------------------------- iFrame Loaded ----------------------------------- */
-  console.log("[*** iFrame Loaded ***]");
-  const frameContent = await elementHandle.contentFrame();
-  if ((await page.$("#closeButton")) !== null) {
-    console.log("[! closing pop up !]");
-    await page.click("#closeButton");
-  }
-  
-  /* ----------------------------------- Filling Document # ----------------------------------- */
-  console.log("[--- Filling Document number & focusing on input ---]");
-  await frameContent.waitForSelector("#formAutenticar\\:numeroDocumento", {timeout: 120000});
-  await frameContent.focus("#formAutenticar\\:numeroDocumento");
-
-  /* ----------------------------------- Filling out input ----------------------------------- */
-  console.log("[ - filling out input - ]");
-  await frameContent.$eval(
-    "#formAutenticar\\:numeroDocumento",
-    (el, value) => (el.value = value),
-    id
-  );
-
-  /* ----------------------------------- continue to password ----------------------------------- */
-  console.log("[ - continue to password - ]");
-  await frameContent.click("#formAutenticar\\:btnSubmitCont");
-  if ((await page.$("#closeButton")) !== null) {
-    console.log("[! closing pop up !]");
-    await page.click("#closeButton");
-  }
-
-  /* ----------------------------------- PASSWORD INPUT ----------------------------------- */
-  console.log("[ --- PASSWORD INPUT --- ]");
-  await frameContent.waitForSelector("#formAutenticar\\:claveVirtual");
-  await page.keyboard.type(password.toString(), { delay: 100 });
-  await frameContent.$eval(
-    "#formAutenticar\\:claveVirtual",
-    (el, value) => (el.value = value),
-    password
-  );
-  if ((await page.$("#closeButton")) !== null) {
-    console.log("[! closing pop up !]");
-    await page.click("#closeButton");
-  }
-
-  /* ----------------------------------- SUBMITING PASSWORD ----------------------------------- */
-  console.log("!!! SUBMITING PASSWORD !!!");
-  await frameContent.click("#formAutenticar\\:btnSubmitCont");
-
-  if ((await page.$("#closeButton")) !== null) {
-    console.log("[! closing pop up !]");
-    await page.click("#closeButton");
-  }
-
-  await frameContent.waitFor(15000);
-  console.log("... waited 15000 ms");
-
-  if ((await page.$("#closeButton")) !== null) {
-    console.log("[! closing pop up !]");
-    await page.click("#closeButton");
-  }
-
-  /* ----------------------------------- PREVENTIVE screenshot & content ----------------------------------- */
-  console.log("[ PREVENTIVE screenshot & content]");
-
-  let pageContent = await page.content();
-  fs.writeFile(
-    "temp/preventiveDaviviendaLoggedIn.html",
-    pageContent,
-    "utf8",
-    err => {
-      if (err) throw err;
-      console.log("preventiveDaviviendaLoggedIn been saved!");
+    /* ----------------------------------- iFrame Loaded ----------------------------------- */
+    console.log("[*** iFrame Loaded ***]");
+    const frameContent = await elementHandle.contentFrame();
+    if ((await page.$("#closeButton")) !== null) {
+      console.log("[! closing pop up !]");
+      await page.click("#closeButton");
     }
-  );
 
-  await page.screenshot({ path: "temp/preventiveDaviviendaLoggedIn.png" });
+    /* ----------------------------------- Filling Document # ----------------------------------- */
+    console.log("[--- Filling Document number & focusing on input ---]");
+    await frameContent.waitForSelector("#formAutenticar\\:numeroDocumento", {
+      timeout: 120000
+    });
+    await frameContent.focus("#formAutenticar\\:numeroDocumento");
 
-  /* ----------------------------------- checking for error ----------------------------------- */
-  console.log("[ --- checking for errors ---]");
-  let data = [],
-    content;
-  if (!frameContent.isDetached()) {
-    if ((await frameContent.$("#divMessageCodigo")) !== null) {
-      console.log("[! ERROR PRESENT !]");
-      data = await frameContent.evaluate(() => {
-        const tds = Array.from(
-          document.querySelectorAll(".tablaMessage tbody tr td")
-        );
-        return tds.map(td => td.textContent);
-      });
-      content = await frameContent.content();
-      await browser.close();
-      console.log("[Sending error to postman or api caller]");
-      if (data.length > 0) {
-        res.send({ code: "already-logged-in", message: data[1] });
-        throw new Error("already-logged-in")
+    /* ----------------------------------- Filling out input ----------------------------------- */
+    console.log("[ - filling out input - ]");
+    await frameContent.$eval(
+      "#formAutenticar\\:numeroDocumento",
+      (el, value) => (el.value = value),
+      id
+    );
+
+    /* ----------------------------------- continue to password ----------------------------------- */
+    console.log("[ - continue to password - ]");
+    await frameContent.click("#formAutenticar\\:btnSubmitCont");
+    if ((await page.$("#closeButton")) !== null) {
+      console.log("[! closing pop up !]");
+      await page.click("#closeButton");
+    }
+
+    /* ----------------------------------- PASSWORD INPUT ----------------------------------- */
+    console.log("[ --- PASSWORD INPUT --- ]");
+    await frameContent.waitForSelector("#formAutenticar\\:claveVirtual");
+    await page.keyboard.type(password.toString(), { delay: 100 });
+    await frameContent.$eval(
+      "#formAutenticar\\:claveVirtual",
+      (el, value) => (el.value = value),
+      password
+    );
+    if ((await page.$("#closeButton")) !== null) {
+      console.log("[! closing pop up !]");
+      await page.click("#closeButton");
+    }
+
+    /* ----------------------------------- SUBMITING PASSWORD ----------------------------------- */
+    console.log("!!! SUBMITING PASSWORD !!!");
+    await frameContent.click("#formAutenticar\\:btnSubmitCont");
+
+    if ((await page.$("#closeButton")) !== null) {
+      console.log("[! closing pop up !]");
+      await page.click("#closeButton");
+    }
+
+    await frameContent.waitFor(15000);
+    console.log("... waited 15000 ms");
+
+    if ((await page.$("#closeButton")) !== null) {
+      console.log("[! closing pop up !]");
+      await page.click("#closeButton");
+    }
+
+    /* ----------------------------------- PREVENTIVE screenshot & content ----------------------------------- */
+    console.log("[ PREVENTIVE screenshot & content]");
+
+    let pageContent = await page.content();
+    fs.writeFile(
+      "temp/preventiveDaviviendaLoggedIn.html",
+      pageContent,
+      "utf8",
+      err => {
+        if (err) throw err;
+        console.log("preventiveDaviviendaLoggedIn been saved!");
+      }
+    );
+
+    await page.screenshot({ path: "temp/preventiveDaviviendaLoggedIn.png" });
+
+    /* ----------------------------------- checking for error ----------------------------------- */
+    console.log("[ --- checking for errors ---]");
+    let content;
+    if (!frameContent.isDetached()) {
+      if ((await frameContent.$("#divMessageCodigo")) !== null) {
+        console.log("[! ERROR PRESENT !]");
+        data = await frameContent.evaluate(() => {
+          const tds = Array.from(
+            document.querySelectorAll(".tablaMessage tbody tr td")
+          );
+          return tds.map(td => td.textContent);
+        });
+        content = await frameContent.content();
+        await browser.close();
+        console.log("[Sending error to postman or api caller]");
+        if (data.length > 0) {
+          res.send();
+          throw { code: "already-logged-in", message: data[1] };
+        }
       }
     }
-  }
 
-  /* ----------------------------------- taking screenshot and grabing content ----------------------------------- */
-  console.log("[closing]");
-  pageContent = await page.content();
-  console.log("[taking daviviendaLoggedInScreenshot]");
-  await page.screenshot({ path: "temp/daviviendaLoggedInScreenshot.png" });
+    /* ----------------------------------- taking screenshot and grabing content ----------------------------------- */
+    console.log("[closing]");
+    pageContent = await page.content();
+    console.log("[taking daviviendaLoggedInScreenshot]");
+    await page.screenshot({ path: "temp/daviviendaLoggedInScreenshot.png" });
 
-  console.log(' --- Writing hmtl file --- ');
-  fs.writeFileSync("temp/daviviendaLoggedIn.html", pageContent, "utf8", err => {
-    if (err) throw err;
-    console.log("---- DaviviendaLoggedIn has been saved!");
-  });
-  
-  /* ----------------------------------- grabing basic info from html file  ----------------------------------- */
-  console.log('[... getting basic info]');
-  // basicInfo()
+    console.log(" --- Writing html file --- ");
+    fs.writeFileSync(
+      "temp/daviviendaLoggedIn.html",
+      pageContent,
+      "utf8",
+      err => {
+        if (err) throw err;
+        console.log("---- DaviviendaLoggedIn has been saved!");
+      }
+    );
 
-  const $ = cheerio.load(pageContent)
-  let test = $('#dashboardform:pagepanel .content-resumen table tbody tr')
-  console.log('test',test);
+    /* ----------------------------------- grabing basic info from html file  ----------------------------------- */
+    console.log("[... getting basic info]");
+    // basicInfo()
 
-  // root.map(async (detailedInfo, i) => {
-  //   const detailedInfoID = detailedInfo.querySelectorAll('td a').id;
-  //   console.log('attributes',detailedInfoID);
-  //   await page.click(detailedInfoID);
-  //   fs.writeFileSync(`temp/${i}acc.hmtl`, page.content(), "utf8", err => {
-  //     if (err) throw err;
-  //     console.log("---- 1acc has been saved!");
-  //   });
-  // })
+    let $ = cheerio.load(pageContent);
+    console.log("detailedInfoAccounts", $(".type-one tbody").html());
 
+    $(".type-one tbody").each(()=>{
+      console.log('text',$(this).find('a').text());
+    })
+    // $.each(()=>{
 
-  /* ----------------------------------- loging out and closing browser ----------------------------------- */
-  if (data.length === 0) {
-    await page.click("#dashboardform\\:cerrarSesion");
-    await page.waitForSelector("#personas-ingresar");
-    await page.screenshot({ path: "temp/logoutScreenshot.png" });
-    fs.writeFileSync("temp/daviviendaLoggedOut.html", content, "utf8", err => {
-      if (err) throw err;
-      console.log("---- daviviendaLoggedOut has been saved!");
-    });
-  }
-  await browser.close();
+    // })
 
-  console.log("[Sending data to postman or api caller]");
-  if (data.length > 0) {
-    res.send({ code: "already-logged-in", message: data[1] });
-  } else {
+    // $.map(async  (i ,el) => {
+    //   const detailedInfoID = el('td a').attr('id');
+    //   console.log('attributes',detailedInfoID);
+    //   await page.click(detailedInfoID);
+    //   fs.writeFileSync(`temp/${i}acc.html`, page.content(), "utf8", err => {
+    //     if (err) throw err;
+    //     console.log(`---- ${i}acc has been saved!`);
+    //   });
+    // })
+
+    /* ----------------------------------- loging out and closing browser ----------------------------------- */
+    if (data.length === 0) {
+      await page.click("#dashboardform\\:cerrarSesion");
+      await page.waitForSelector("#personas-ingresar");
+      await page.screenshot({ path: "temp/logoutScreenshot.png" });
+      fs.writeFileSync(
+        "temp/daviviendaLoggedOut.html",
+        content,
+        "utf8",
+        err => {
+          if (err) throw err;
+          console.log("---- daviviendaLoggedOut has been saved!");
+        }
+      );
+    }
+    await browser.close();
+
+    console.log("[Sending data to postman or api caller]");
     res.sendFile(path.resolve("temp/daviviendaLoggedInScreenshot.png"));
-  }
+    console.log("[-- end --]");
 
-  console.log('[-- end --]')
+  } catch (error) {
+    console.log('error', error);
+    /* ----------------------------------- loging out and closing browser ----------------------------------- */
+    if (data.length === 0) {
+      await page.click("#dashboardform\\:cerrarSesion");
+      await page.waitForSelector("#personas-ingresar");
+      await page.screenshot({ path: "temp/logoutScreenshot.png" });
+      fs.writeFileSync(
+        "temp/daviviendaLoggedOut.html",
+        content,
+        "utf8",
+        err => {
+          if (err) throw err;
+          console.log("---- daviviendaLoggedOut has been saved!");
+        }
+      );
+    }
+    await browser.close();
+
+    console.log("[Sending error to postman or api caller]");
+    if (data.length > 0) {
+      res.send({ code: "already-logged-in", message: data[1] });
+    } else {
+      res.send({ code: "server error", message: error });
+    }
+
+    console.log("[-- end with error --]");
+  }
 };
 
 const basicInfo = async (req, res) => {
   console.log("[ --- parsing report --- ]");
-  const davivienda = fs.readFileSync(path.resolve("temp/daviviendaLoggedIn.html"), "utf8", (err) => {
-    if (err) throw err;
-  });
+  const davivienda = fs.readFileSync(
+    path.resolve("temp/daviviendaLoggedIn.html"),
+    "utf8",
+    err => {
+      if (err) throw err;
+    }
+  );
 
-  let root = HTMLParser.parse(davivienda)
-  root = root.querySelectorAll('#dashboardform:pagepanel .content-resumen table tbody tr')
+  /* TODO: migrate to cheerio */
+  let root = HTMLParser.parse(davivienda);
+  root = root.querySelectorAll(
+    "#dashboardform:pagepanel .content-resumen table tbody tr"
+  );
 
-  let servicesInfo = []
+  let servicesInfo = [];
   root.forEach(services => {
-    name = services.querySelectorAll('td table tbody tr td')[0].innerHTML
-    let account = {}
-    if (name.includes('Cuenta')) {
+    name = services.querySelectorAll("td table tbody tr td")[0].innerHTML;
+    let account = {};
+    if (name.includes("Cuenta")) {
       account = {
-        available: services.querySelectorAll('td table tbody tr td')[1].innerHTML,
-        savings: services.querySelectorAll('td table tbody tr td')[3].innerHTML,
-        total: services.querySelectorAll('td table tbody tr td')[5].innerHTML
-      }
+        available: services.querySelectorAll("td table tbody tr td")[1]
+          .innerHTML,
+        savings: services.querySelectorAll("td table tbody tr td")[3].innerHTML,
+        total: services.querySelectorAll("td table tbody tr td")[5].innerHTML
+      };
     } else {
       account = {
-        available: services.querySelectorAll('td table tbody tr td')[1].innerHTML,
+        available: services.querySelectorAll("td table tbody tr td")[1]
+          .innerHTML,
         minimumPayment: {
-          ammount: services.querySelectorAll('td table tbody tr td')[2].innerHTML,
-          dateDue: services.querySelectorAll('td table tbody tr td')[3].innerHTML
+          ammount: services.querySelectorAll("td table tbody tr td")[2]
+            .innerHTML,
+          dateDue: services.querySelectorAll("td table tbody tr td")[3]
+            .innerHTML
         },
-        debt: services.querySelectorAll('td table tbody tr td')[4].innerHTML,
-        totalPayment: services.querySelectorAll('td table tbody tr td')[5].innerHTML
-      }
+        debt: services.querySelectorAll("td table tbody tr td")[4].innerHTML,
+        totalPayment: services.querySelectorAll("td table tbody tr td")[5]
+          .innerHTML
+      };
     }
-    servicesInfo = [...servicesInfo, {name, account}]
+    servicesInfo = [...servicesInfo, { name, account }];
   });
-  console.log('servicesInfo', servicesInfo);
+  console.log("servicesInfo", servicesInfo);
 
-  res.send(servicesInfo)
+  res.send(servicesInfo);
 };
 
 module.exports = { getReports, basicInfo };
