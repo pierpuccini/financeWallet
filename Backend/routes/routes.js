f4eb602f1cf6e812eb5150bc3d301108ae6d74cb
pierpuccini <46226655+pierpuccini@users.noreply.github.com>
2020-03-21 15:16:56 -0500

    Implemented folderstucture creation

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -5,8 +5,12 @@
 |File for handle route that api call
    
 */
+/* Dependancies */
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
+
+/* Controllers */
 const daviviendaController = require("../controller/daviviendaController")
 
 // Get Message
@@ -15,6 +19,14 @@ router.post("/text", (req, res) => {
   console.log("[started]");
   res.send({ message: text });
 });
+
+/* Temp path creation for davi */
+fs.mkdirSync("./temp/davi",{recursive: true});
+fs.mkdirSync("./temp/davi/in",{recursive: true});
+fs.mkdirSync("./temp/davi/movements",{recursive: true});
+fs.mkdirSync("./temp/davi/overview",{recursive: true});
+//TODO: THIS WILL BE REMOVED
+fs.mkdirSync("./temp/davi/preventive",{recursive: true});
 
 router.get("/davivienda-get-reports", daviviendaController.getReports);
 router.get("/test", daviviendaController.test);
