79bff57e8c1f5a766bb950157f5cafe6c3131c19
Pier Puccini <pierpuccini@users.noreply.github.com>
2021-02-26 16:30:08 -0500

    Exported login item into it's own component

---
+++ Frontend/src/components/UI/login/Login.js
@@ -0,0 +1,33 @@
+/* React */
+import React from "react";
+/* Firebase */
+import { useFirebase } from "react-redux-firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const Login = () => {
+  const firebase = useFirebase();
+
+  return (
+    <StyledFirebaseAuth
+      uiConfig={{
+        signInFlow: "redirect",
+        signInSuccessUrl: "/dashboard",
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          {
+            provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            recaptchaParameters: {
+              type: "image", // 'audio'
+              size: "invisible", // 'invisible' or 'compact'
+              badge: "inline", //' bottomright' or 'inline' applies to invisible.
+            },
+            defaultCountry: "CO",
+          },
+        ],
+      }}
+      firebaseAuth={firebase.auth()}
+    />
+  );
+};
+
+export default Login;
--- Frontend/src/containers/loginPage/LoginPage.js
+++ Frontend/src/containers/loginPage/LoginPage.js
@@ -5,13 +5,12 @@ import { Redirect } from "react-router-dom"; // if you use react-router
 /* Redux */
 import { useSelector } from "react-redux";
 /* Firebase */
-import { useFirebase, isLoaded, isEmpty } from "react-redux-firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { isLoaded, isEmpty } from "react-redux-firebase";
 /* Components */
 import Loader from "../../components/UI/loader/PngLoader";
+import Login from "../../components/UI/login/Login";
 
 const LoginPage = () => {
-  const firebase = useFirebase();
   const auth = useSelector((state) => state.firebase.auth);
 
   let element = (
@@ -21,27 +20,7 @@ const LoginPage = () => {
   );
 
   if (isLoaded(auth) && isEmpty(auth)) {
-    element = (
-      <StyledFirebaseAuth
-        uiConfig={{
-          signInFlow: "redirect",
-          signInSuccessUrl: "/dashboard",
-          signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            {
-              provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-              recaptchaParameters: {
-                type: "image", // 'audio'
-                size: "invisible", // 'invisible' or 'compact'
-                badge: "inline", //' bottomright' or 'inline' applies to invisible.
-              },
-              defaultCountry: "CO",
-            },
-          ],
-        }}
-        firebaseAuth={firebase.auth()}
-      />
-    );
+    element = <Login />;
   } else if (isLoaded(auth) && !isEmpty(auth)) {
     element = <Redirect to="/dashboard" />;
   }
