184a6e441b6e0c1695f2f712d30fafe8ca9b9425
Pier Puccini <pierpuccini@users.noreply.github.com>
2021-02-26 18:12:02 -0500

    Added dashboard

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,22 @@
 /* React */
-import React, { useMemo } from "react";
+import React, { useMemo, cloneElement } from "react";
+import PropTypes from "prop-types";
 /* React Router */
 import { Switch, Route, Redirect } from "react-router-dom";
 /* Redux */
 import { useSelector } from "react-redux";
 /* Firebase */
 import { isLoaded } from "react-redux-firebase";
+/* Material Imports */
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import Container from "@material-ui/core/Container";
 /* Assets */
 /* Containers */
 import LoginPage from "./containers/loginPage/LoginPage";
+import Dashboard from "./containers/dashboard/Dashboard";
 /* Components */
 import Loader from "./components/UI/loader/PngLoader";
 import PrivateRoute from "./components/UI/privateRoute/PrivateRoute";
@@ -30,7 +38,7 @@ const AuthIsLoaded = ({ children }) => {
   return children;
 };
 
-const App = () => {
+const App = (props) => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   const theme = useMemo(
@@ -46,20 +54,48 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AuthIsLoaded>
-        <Switch>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <PrivateRoute path="/dashboard">
-            <div>Protected content Auth is Loaded</div>
-            {/* Rest of App Components */}
-          </PrivateRoute>
-          <Redirect to="/dashboard" />
-        </Switch>
-      </AuthIsLoaded>
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">Scroll to Elevate App Bar</Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
+      <Container>
+        <AuthIsLoaded>
+          <Switch>
+            <Route path="/login" component={LoginPage}></Route>
+            <PrivateRoute
+              path="/dashboard"
+              component={Dashboard}
+            ></PrivateRoute>
+            <Redirect to="/dashboard" />
+          </Switch>
+        </AuthIsLoaded>
+      </Container>
     </ThemeProvider>
   );
+};
+
+const ElevationScroll = (props) => {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+};
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
 };
 
 export default App;
